ce172337c57cab2ea0f7d1673d2b7878cc9d67e4
maxcliff <33406934+maxcliff@users.noreply.github.com>
2018-04-11 17:23:19 -0400

    Update example.js

--- example.js
+++ example.js
@@ -1,1 +1,27 @@
+const acronym = require('./');
 
+acronym('for your information') 
+  .then(data => {
+    console.log(data)
+  }) 
+   .catch(error => {
+     console.log(error)
+   }) 
+  
+
+acronym("for your information")
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+
+acronym('talk to you later')
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => {
+    console.log(error)
+  })
